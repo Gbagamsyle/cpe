f9dcdff6cc373b8c6054cf28d2d30563b2a42dad
Gbagamsyle <gbagamscott@gmail.com>
2026-05-27 12:11:27 +0100

    Load component CSS from root layout to avoid missing CSS chunk link errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/variables.css";
 import "./styles/common.css";
+import "../components/Nav.css";
+import "../components/Home.css";
+import "../components/Programs.css";
+import "../components/News.css";
+import "../components/Contact.css";
+import "../components/Faculty.css";
+import "../components/Footer.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
